6c52f4582b5b17cb30c456f572e78108c58e7e5b
yrslv_v <yarikkoleba@gmail.com>
2024-08-05 13:42:57 -0700

    Remove submodule configuration and re-add frontend as a regular directory

---
+++ frontend/build/precache-manifest.1ef813639c857ffbc611305ecdf3cadc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3d422ce717ddd2951ed4927583fd10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0506af79ec4d36b6079e",
+    "url": "/static/css/main.21d50e5c.chunk.css"
+  },
+  {
+    "revision": "58c7908faaa4ca608d22",
+    "url": "/static/js/2.c9554f55.chunk.js"
+  },
+  {
+    "revision": "311fdf2176b69a5a30ee",
+    "url": "/static/js/3.2ee60a5f.chunk.js"
+  },
+  {
+    "revision": "0506af79ec4d36b6079e",
+    "url": "/static/js/main.45d1bb6b.chunk.js"
+  },
+  {
+    "revision": "30ef0fa35508797b6fe7",
+    "url": "/static/js/runtime~main.b22a5fca.js"
+  }
+]);
